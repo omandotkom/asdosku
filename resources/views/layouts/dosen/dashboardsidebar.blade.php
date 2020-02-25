6762b6ee5fa3a76b78775059d055f494bf857bde
Khalid Abdurrahman <omandotkom@gmail.com>
2020-02-25 12:54:55 +0700

    update dashboard link di role dosen

--- resources/views/layouts/dosen/dashboardsidebar.blade.php
+++ resources/views/layouts/dosen/dashboardsidebar.blade.php
@@ -21,7 +21,7 @@
 </script>
 <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
     <!-- Sidebar - Brand -->
-    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="{{route('indexdosen')}}">
+    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="{{route('dashboard')}}">
         <div class="sidebar-brand-icon rotate-n-15">
             <i class="fas fa-laugh-wink"></i>
         </div>
