530d40aee81edd38edfdb102987ff8620cd3ffeb
Khalid Abdurrahman <omandotkom@gmail.com>
2020-07-15 17:32:33 +0700

    registerf mechanism

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -10,6 +10,7 @@ use App\Http\Middleware\Dosen;
 use App\Http\Middleware\HRD;
 use App\Http\Middleware\Marketing;
 use App\Http\Middleware\Operational;
+use App\Http\Middleware\RegisterMiddlewareF;
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
 
 class Kernel extends HttpKernel
@@ -43,6 +44,7 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            RegisterMiddlewareF::class
         ],
 
         'api' => [
@@ -76,7 +78,8 @@ class Kernel extends HttpKernel
         'operational' => Operational::class,
         'asdos' => Asdos::class,
         'hrd' => HRD::class,
-        'marketing' => Marketing::class
+        'marketing' => Marketing::class,
+        'registerf' => RegisterMiddlewareF::class
     ];
 
     /**
