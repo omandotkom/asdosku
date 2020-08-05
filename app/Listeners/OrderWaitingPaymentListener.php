9c7355e460e2bd062c5659097a9503cafe45ce53
vandy ahmad <vandyahmad2404@gmail.com>
2020-08-05 07:21:55 +0700

    update status asdos

--- app/Listeners/OrderWaitingPaymentListener.php
+++ app/Listeners/OrderWaitingPaymentListener.php
@@ -9,6 +9,7 @@ use App\Notifications\MailContent;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
 use App\Pemberitahuan;
+use App\Cost;
 class OrderWaitingPaymentListener
 {
     /**
@@ -30,10 +31,13 @@ class OrderWaitingPaymentListener
     public function handle(OrderWaitingPayment $event)
     {
         //$mailDosen = new MailContent("Notifikasi Asdosku","Layanan dengan kode transaksi ".$event->transaction->id." telah selesai. Anda memiliki tagihan yang harus dibayar, silahkan login ke Asdosku untuk mengunggah bukti pembayaran","Masuk ke Asdosku",route('login'));
+        $total_cost = Cost::where('transaction_id',$event->transaction->id)->sum('nominal');
+        $total= $total_cost+$event->transaction->biaya;
+
         Pemberitahuan::create([
             'to' => $event->dosen->email,
             'subject' => 'Notifikasi Asdosku',
-            'judul' => "Layanan dengan kode transaksi ".$event->transaction->id." telah selesai. Anda memiliki tagihan yang harus dibayar sebesar Rp.".number_format($event->transaction->biaya,2).", silahkan login ke Asdosku untuk mengunggah bukti pembayaran",
+            'judul' => "Layanan dengan kode transaksi ".$event->transaction->id." telah selesai. Anda memiliki tagihan yang harus dibayar sebesar Rp.".number_format($total,2).", silahkan login ke Asdosku untuk mengunggah bukti pembayaran",
             'isi' => "Lihat Tagihan Anda",
             'url' => route('login'),
             'status' => 'unsent'
